3995c5bdd1731a017fe4530ddb1b354c9f553dee
Pablo Cru <122596837+pabcrudel@users.noreply.github.com>
2023-01-23 20:19:12 +0100

    Añadidos comentarios a los scripts

--- scripts/crea-ies.sh
+++ scripts/crea-ies.sh
@@ -11,32 +11,35 @@
 
 clear
 
-mkdir /IES
+# Guardada la ruta actual para volver a esta cuando termine el script.
+ruta_actual=$(pwd)
 
+# Creada la carpeta en la raíz del disco.
+mkdir IES
+
+# Acceder a esta y crear las carpetas de alumno y profesor.
 cd IES
 mkdir  Alumnos
 mkdir Profesores
 
+# Acceder a estas, crear las carpetas de los superiores y crear un fichero con infromacion de alumnos de cada una.
 cd Alumnos
 mkdir ASIR
-echo "Nombre: Marta; Edad: 20" > ASIR/marta.txt
-echo "Nombre: Mario; Edad:23" > ASIR/mario.txt
+printf "Nombre: Marta\nEdad: 20" > ASIR/marta.txt
+printf "Nombre: Mario\nEdad:23" > ASIR/mario.txt
 
 mkdir DAW
-echo "Nombre: Pablo; Edad: 26" > DAW/pablo.txt
-echo "Nombre: Dani; Edad: 19" > DAW/dani.txt
+printf "Nombre: Pablo\nEdad: 26" > DAW/pablo.txt
+printf "Nombre: Dani\nEdad: 19" > DAW/dani.txt
 
 cd ../Profesores
 mkdir ASIR
-echo "Nombre: Elena; Clase: Info4" > ASIR/elena.txt
-echo "Nombre: Marcos; Clase: Info4" > ASIR/marcos.txt
+printf "Nombre: Elena\nClase: Bases de Datos" > ASIR/elena.txt
+printf "Nombre: Marcos\nClase: Sistemas Informaticos" > ASIR/marcos.txt
 
 mkdir DAW
-echo "Nombre: Pascual; Clase: Info3" > DAW/pascual.txt
-echo "Nombre: Jorge; Clase: Info3" > DAW/jorge.txt
+printf "Nombre: Pascual\nClase: Lenguajes de MArcas" > DAW/pascual.txt
+printf "Nombre: Jorge\nClase: Entornos de Desarrollo" > DAW/jorge.txt
 
-cd ../..
-
-
-
-
+# Volver a la ruta original.
+cd $ruta_actual
---
+++ scripts/get-pwd-change-dir.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Script que almacenar la ruta actual en una variable utilizando el comando pwd (present working directory),
+# luego utilizar esa variable para cambiar a esa ruta en un momento posterior utilizando el comando cd (change directory).
+#
+#-------------------------------------
+# Author: Pablo Cru
+# Github: https://github.com/pabcrudel
+#-------------------------------------
+
+clear
+
+# Almacenar la ruta actual en una variable
+ruta_actual=$(pwd)
+
+# Realizar algunas acciones
+echo "Haciendo algo en otra ruta"
+cd ./scripts
+pwd
+
+# Volver a la ruta original
+cd $ruta_actual
