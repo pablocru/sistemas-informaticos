863606947c04d5109021c2ca6e6ba290f982be04
pabcrudel <pabcrudel@gmail.com>
2023-01-25 13:34:23 +0100

    Eliminados los carriage return characters de los scripts editados en windows

--- scripts/test.sh
+++ scripts/test.sh
@@ -11,19 +11,20 @@
 
 clear
 
-# Creada la carpeta en la raíz del disco. Para que funcione correctamente debes ser administrador por lo que deberias usar "sudo".
-mkdir /IES
+# Guardada la ruta actual para volver a esta cuando termine el script.
+ruta_actual=$(pwd)
+
+# Creada la carpeta en la raíz del disco.
+mkdir IES
 
 # Acceder a esta y crear las carpetas de alumno y profesor.
-cd /IES
+cd IES
 mkdir  Alumnos
 mkdir Profesores
 
 # Acceder a estas, crear las carpetas de los superiores y crear un fichero con infromacion de alumnos de cada una.
 cd Alumnos
 mkdir ASIR
-
-#Se diferencia de "echo" en que respeta los caracteres de escape para añadir, por ejemplo, saltos de linea al archivo
 printf "Nombre: Marta\nEdad: 20" > ASIR/marta.txt
 printf "Nombre: Mario\nEdad:23" > ASIR/mario.txt
 
@@ -39,3 +40,6 @@ printf "Nombre: Marcos\nClase: Sistemas Informaticos" > ASIR/marcos.txt
 mkdir DAW
 printf "Nombre: Pascual\nClase: Lenguajes de MArcas" > DAW/pascual.txt
 printf "Nombre: Jorge\nClase: Entornos de Desarrollo" > DAW/jorge.txt
+
+# Volver a la ruta original.
+cd $ruta_actual
