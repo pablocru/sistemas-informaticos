d85c299fcb594f14978095b5c055d17881555aba
pabcrudel <pabcrudel@gmail.com>
2023-02-02 13:02:01 +0100

    Creada una calculador que utiliza un if-else para cambiar entre operadores

---
+++ scripts/mycalc.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# Script que lee 3 parametros y realiza un calculo en funcion de estos:
+# -Parametro 1: primer numero para la operacion.
+# -Parametro 2: operador que decide que calculo realizar.
+# -Parametro 3: segundo numero para la operacion.
+# Aunque seria mas correcto usar un switch, en este caso se utiliza una
+# estructura if-else para practicar lo dado en el temario.
+# --------------------------------------------------------------------
+# Author: Pablo Cru
+# GitHub: https://github.com/pabcrudel
+# --------------------------------------------------------------------
+
+clear
+
+if [ $2 == mas ]; then
+	res=`expr $1 + $3`
+	echo La suma de $1 y $3 da $res
+elif [ $2 == men ]; then
+	res=`expr $1 - $3`
+	echo La resta de $1 y $3 da $res
+elif [ $2 == mul ]; then
+	res=`expr $1 \* $3`
+	echo La multiplicación de $1 y $3 da $res
+elif [ $2 == div ]; then
+	res=`expr $1 / $3`
+	echo La división de $1 y $3 da $res
+else
+	echo $2 no es un operador valido. Los validos son mas, men, mul, div.
+fi
