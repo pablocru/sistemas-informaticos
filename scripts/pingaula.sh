1d2f80f8ff2acff80df02a26b45361612c266b6d
pabcrudel <pabcrudel@gmail.com>
2023-02-13 12:48:25 +0100

    Iniciado el script con su descripcion

---
+++ scripts/pingaula.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+# Script que verifica los ordenadores del aula que estan encendidos y tienen comunicacion.
+#
+# --------------------------------------------------------------------
+# Author: Pablo Cru
+# GitHub: https://github.com/pabcrudel
+# --------------------------------------------------------------------
+
+clear
+echo
+
+
+
