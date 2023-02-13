d54d84eea83090e9e527ed15d924e466378409d7
pabcrudel <pabcrudel@gmail.com>
2023-02-13 13:45:23 +0100

    Creado script para ver que ordenadores estan opertivos en la red

--- scripts/pingaula.sh
+++ scripts/pingaula.sh
@@ -2,13 +2,32 @@
 #
 # Script que verifica los ordenadores del aula que estan encendidos y tienen comunicacion.
 #
-# --------------------------------------------------------------------
+# ----------------------------------------------------------------------------------------
 # Author: Pablo Cru
 # GitHub: https://github.com/pabcrudel
-# --------------------------------------------------------------------
+# ----------------------------------------------------------------------------------------
 
 clear
 echo
 
+echo -n "Dame el numero de PC inicial: "
+read init
+echo -n "Dame el numero de PC final: "
+read fin
 
+echo
+echo Probando conectar con el aula 1, PC\'S desde $init hasta el $fin
+echo
 
+msg="El ordenador 10.2.1."
+
+for i in `seq $init $fin`
+do
+	comand=`ping -c 1 10.2.1.$i`
+	if [ $? -eq 0 ];then
+	echo $msg$i esta en red
+	else
+	echo $msg$i NO se encuentra operativo
+	fi
+done
+echo
