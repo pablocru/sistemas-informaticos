ff1b6e0fc9b4d7fc53a045b89ad5cfbef96d8db8
pabcrudel <pabcrudel@gmail.com>
2023-03-23 13:28:02 +0100

    Script que filtra datos

---
+++ busca-personas-grep/buscapersonas.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+#
+# Lee el archivo "personas.dat" y muestra la lista de personas completa. Luego, el usuario podra hacer 3 acciones:
+# - Mostrar las personas llamadas "Marcos"
+# - Mostrar las personas casadas
+# - Almacenar en un fichero (datos.txt) los profesores y las personas con 38 años
+#
+# --------------------------------------------------------------------
+# Author: Pablo Cru
+# GitHub: https://github.com/pabcrudel
+# --------------------------------------------------------------------
+
+clear
+echo
+
+cat personas.dat
+echo
+
+read
+
+grep "Marcos" personas.dat
+echo
+
+read
+
+grep "SI" personas.dat
+echo
+
+read
+
+firstLine="------------------------------"
+fileName="datos.txt"
+
+echo $firstLine > $fileName
+echo "Profesores tenemos:" >> $fileName
+echo >> $fileName
+grep "Profesor" personas.dat >> $fileName
+
+echo $firstLine >> $fileName
+echo "Personas con 38 años tenemos:" >> $fileName
+echo >> $fileName
+grep "38" personas.dat >> $fileName
+
+cat $fileName
+echo
+
+
+
